c28dd5e1884c0aed1e2cbc810d7044bd8e75c614
shoji-k <shoji.kobayashi.0125@gmail.com>
2014-07-30 22:08:59 +0900

    add genganashura and kurogane

---
+++ genganashura.rb
@@ -0,0 +1,24 @@
+require 'open-uri'
+require 'nokogiri'
+require 'hipchat'
+
+require './config'
+
+doc = Nokogiri::HTML(open("http://urasunday.com/kengan/"))
+msg = doc.css('h3.h3Date')[0].text
+
+filename = "gengan-date.txt"
+if File.exist?(filename) == false
+  File.write(filename, "no data")
+end
+prev_msg = File.read(filename, :encoding => Encoding::UTF_8)
+
+if prev_msg != msg
+  client = HipChat::Client.new($token)
+  # client['room'].send("ruby", "@all test message from ruby", :message_format => 'text', :color => 'purple', :notify => 1)
+
+  client[$room_name].send("gengan", msg, :notify => 1)
+
+  File.write(filename, msg)
+end
+
