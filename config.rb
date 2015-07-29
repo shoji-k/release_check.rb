e7de120fa138cb7011dfd05d3c82c361db022335
kobayashi <shoji.kobayashi.0125@gmail.com>
2015-07-29 19:14:31 +0900

    add config.rb

---
+++ config.rb
@@ -0,0 +1,14 @@
+
+$notice_hipchat = false
+# Hipchat token
+$token = "###################"
+# Hipchat room name
+$room_name = '####'
+
+# slack
+$notice_slack = true
+$slack_room_name = '#roomname'
+Slack.configure do |config|
+  config.token = 'xxxx'
+end
+
