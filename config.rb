c72ee52e9aae8ce261cc25b50c3e018c546a64e7
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2016-01-16 13:52:55 +0900

    mask slack config

--- config.rb
+++ config.rb
@@ -7,8 +7,8 @@ $room_name = '####'
 
 # slack
 $notice_slack = true
-$slack_room_name = '#general'
+$slack_room_name = '#sample'
 Slack.configure do |config|
-  config.token = 'xoxp-3309287953-3309287955-8348890049-bd6a1c'
+  config.token = '##########'
 end
 
